ad0ed892f4ac0abcab6709a873c6ef72ae361068
Leonardo Guarilha <lsguarilha01@gmail.com>
2020-08-05 17:15:47 -0300

    Finish frontend web

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,39 +1,55 @@
-import React from "react";
+import React, { useCallback } from "react";
 
 import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
 
 import "./styles.css";
+import api from "../../services/api";
 
-const TeacherItem: React.FC = () => {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: number;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  const createNewConnection = useCallback(() => {
+    api.post("connections", {
+      user_id: teacher.id,
+    });
+  }, [teacher.id]);
+
   return (
     <article className="teacher-item">
       <header>
-        <img
-          src="https://avatars2.githubusercontent.com/u/39388688?s=460&u=3476f0244eb92639f1e5fa7c0f14f0086bd272d8&v=4"
-          alt="Leonardo Guarilha"
-        />
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
-          <strong>Leonardo Guarilha</strong>
-          <span>Química</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et veritatis
-        eligendi nesciunt ratione accusantium laborum quas <br />
-        <br />
-        officiis sint in eius similique numquam voluptatem, odio, facere
-        consequatur suscipit, dolorem labore neque.
-      </p>
+      <p>{teacher.bio}</p>
 
       <footer>
         <p>
           Preço/hora
-          <strong>R$80.00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a
+          target="_blank"
+          onClick={createNewConnection}
+          href={`https://wa.me/${teacher.whatsapp}`}
+        >
           <img src={whatsappIcon} alt="Whatsapp" />
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
